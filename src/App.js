6efd4cb16965955f0eac4436e51a7242fb6940dd
JennyViannay <jenny.viannay75@gmail.com>
2019-12-13 00:06:29 +0100

    front for upload files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import './style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    selectedFile: []
+  }
+  
+  onChangeHandler = (e) => {
+    this.setState({selectedFile: e.target.files})
+  }
+
+  onClickHandler = () => {
+    const data = new FormData() 
+    data.append('file', this.state.selectedFile)
+    axios.post("http://localhost:4000/uploadfile", data)
+      .then(res => { 
+        console.log(res.data)
+      })
+  }
+
+  render(){
+    console.log(this.state)
+    return (
+      <div className="container my-5">
+        <div className="row">
+          <div className="col-12 text-center my-3">
+              <h1 className="">UploadFile</h1>
+          </div>
+          <div className="offset-3 col-6">
+            <div className="form-group files">
+              <input type="file" name="file" multiple className="form-control" onChange={this.onChangeHandler}/>  
+            </div>  
+            <div className="offset-3 col-6 text-center my-3">
+              <button type="button" className="btn btn-primary btn-block" onClick={this.onClickHandler}>Upload</button>
+            </div>
+          </div>  
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
