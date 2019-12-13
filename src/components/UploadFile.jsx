b467a5bc25fb42bd2e3d6d92cce722eecf5c355e
JennyViannay <jenny.viannay75@gmail.com>
2019-12-13 23:15:26 +0100

    fix drag &drop

--- src/components/UploadFile.jsx
+++ src/components/UploadFile.jsx
@@ -1,19 +1,20 @@
-import React from "react";
-import axios from "axios";
-import FlashMessage from "react-flash-message";
+import React from "react"
+import axios from "axios"
+import FlashMessage from "./FlashMsg"
+import DragAndDrop from './DragAndDrop'
 
 class UploadFile extends React.Component {
     state = {
-        files: '',
+        files: [],
         message: {
-            type : '',
-            text : ''
+            type: '',
+            text: ''
         }
-    };
+    }
 
     onChangeHandler = e => {
-        this.setState({ files: e.target.files });
-    };
+        this.setState({ files: e.target.files })
+    }
 
     onClickHandler = e => {
         e.preventDefault();
@@ -25,60 +26,84 @@ class UploadFile extends React.Component {
         axios
             .post("http://localhost:5000/upload/file", data)
             .then(res => {
-                console.log('ok')
-                this.setState({ message: {
-                    type: 'success',
-                    text: 'Files uploaded with success'
-                }}, () => this.state)
+                this.setState({
+                    message: {
+                        type: 'success',
+                        text: 'Files uploaded with success',
+                        isVisible: true
+                    }
+                }, () => this.state)
             })
             .catch(error => {
-                console.log('error')
-                this.setState({ message: {
-                    type: 'error',
-                    text: 'Error, try again'
-                }}, () => this.state)
+                this.setState({
+                    message: {
+                        type: 'error',
+                        text: 'Error, try again'
+                    }
+                }, () => this.state)
             });
-    };
+    }
+
+    reset() {
+        window.location.reload();
+    }
+
+    handleDrop = (files) => {
+        let fileList = this.state.files
+        for (var i = 0; i < files.length; i++) {
+          if (!files[i]) return
+          fileList.push(files[i])
+        }
+        this.setState({files: fileList})
+    }
 
     render() {
+        console.log(this.state)
         return (
-            <div>
+            <>
+                <div className="row">
+                    <FlashMessage
+                        duration={10000}
+                        persistOnHover={true}
+                        type={this.state.message.type}
+                        message={this.state.message.text}
+                        close={() => this.reset()} />
+                </div>
                 <div className="row">
                     <div className="col-12 text-center my-3">
                         <h1 className="">Upload your Files here</h1>
                     </div>
-                    <div className="col-12 text-center my-3">
-                        <FlashMessage duration={20000} persistOnHover={true}>
-                            <p>{this.state.message.type === 'success' ? `${this.state.message.text}` : `${this.state.message.text}`}</p>
-                        </FlashMessage>
+                </div>
+                <div className="input-group">
+                    <div className="input-group-prepend">
+                        <span className="input-group-text"><i className="fas fa-upload mx-1"></i></span>
+                    </div>
+                    <div className="custom-file">
+                        <input type="file" className="custom-file-input" name="file" multiple onChange={this.onChangeHandler} />
+                        <label className="custom-file-label">Choose file</label>
+                    </div>
+                    <button
+                        type="submit"
+                        className="btn btn-primary btn-block my-4"
+                        onClick={this.onClickHandler}>
+                        Upload
+                    </button>
+                </div>
+                <div>
+                    <DragAndDrop handleDrop={this.handleDrop}>
+                        <div className="content-dd"></div>
+                    </DragAndDrop>
+                </div>
+                <div>
+                    <div className="row">
+                        <ul>
+                            {this.state.files && this.state.files.map((file,i) =>  <li key={i}>{file.name}</li>)}
+                        </ul>
                     </div>
                 </div>
-                <form method="POST" encType="multipart/form-data" action="uploadfile">
-                    <div className="row">
-                        <div className="offset-3 col-6">
-                            <div className="form-group files">
-                                <input
-                                    type="file"
-                                    name="file"
-                                    multiple
-                                    className="form-control"
-                                    onChange={this.onChangeHandler}
-                                />
-                            </div>
-                            <div className="offset-3 col-6 text-center my-3">
-                                <button
-                                    type="submit"
-                                    className="btn btn-primary btn-block"
-                                    onClick={this.onClickHandler}>
-                                    Upload
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        );
+            </>
+        )
     }
 }
 
-export default UploadFile;
+export default UploadFile
